b44ddf459cf41ff24c0d3475e5632c99f50f4c6b
Siddharth <siddharthb2481@gmail.com>
2016-12-16 21:29:36 -0800

    Modified Palindrome.java so that it takes words as input and checks to see if they are palindromes

--- Test/Palindrome.java
+++ Test/Palindrome.java
@@ -1,25 +1,26 @@
 import java.util.Scanner;
 public class Palindrome {
 	public static void main(String[] args){
-		System.out.println("enter a number to check if it is a palindrome");
+		System.out.println("enter a word to see if it is a palindrome");
 		Scanner in = new Scanner(System.in);
-		int userNumber = in.nextInt();
-		check(userNumber);
+		String userInput = in.next();
+		check(userInput);
 	}
-	
-	public static void check(int number){
-		StringBuffer s =  new StringBuffer(Integer.toString(number));
-		int size = 	s.length()-1;
-		StringBuffer palindrome = new StringBuffer("");
-		for(int i = size; i >= 0; i--){
-			palindrome.append(s.charAt(i));
-		}
 
-		if(palindrome.toString().equals(s.toString())){
-			System.out.println("The number you entered is a palindrome");
+	public static void check(String input){
+		boolean isPalindrome = true;
+		int n = input.length();
+		  for (int i = 0; i < (n/2); ++i) {
+		     if (input.charAt(i) != input.charAt(n - i - 1)) {
+		         isPalindrome = false;
+		     }
+		  }
+		if(isPalindrome){
+			System.out.println("The word you entered is a palindrome");
 		}
 		else{
-			System.out.println("The number you entered is not a palindrome");
+			System.out.println("The word you entered is not a palindrome");
 		}
 	}
 }
+
